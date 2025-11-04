62b54c8b41f62b4014dbd7a6b4f55872a887ab42
Angu Chen <anguchen96@gmail.com>
2025-11-05 07:40:27 +1300

    add migration for user

---
+++ server/db/migrations/20251104183538_user.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable('user', (table) => {
+    table.increments('id')
+    table.number('auth_id')
+    table.string('username')
+    table.string('fullname')
+    table.string('location')
+    table.string('img')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTable('user')
+}
