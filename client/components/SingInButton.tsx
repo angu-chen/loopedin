ab4326d39fa4b35d2be5f45f1f8901cbf98d361a
Angu Chen <anguchen96@gmail.com>
2025-11-05 12:15:27 +1300

    created a sign in button with no styling as or yet

---
+++ client/components/SingInButton.tsx
@@ -0,0 +1,32 @@
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated'
+import { useAuth0 } from '@auth0/auth0-react'
+
+function SignInButton() {
+  const authData = useAuth0()
+
+  const user = {
+    nickname: authData.user?.nickname,
+  }
+
+  const handleSignOut = () => {
+    authData.logout()
+  }
+
+  const handleSignIn = () => {
+    authData.loginWithPopup()
+  }
+
+  return (
+    <div>
+      <IfAuthenticated>
+        {user && <p>Signed in as: {user?.nickname}</p>}
+        <button onClick={handleSignOut}>Sign out</button>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <button onClick={handleSignIn}>Sign in</button>
+      </IfNotAuthenticated>
+    </div>
+  )
+}
+
+export default SignInButton
