9f361d2c11811e3d6e3d74a9312d5a7088b08524
Angu Chen <anguchen96@gmail.com>
2025-11-06 12:49:57 +1300

    changed form so it updates a user object

--- client/components/SignUpForm.tsx
+++ client/components/SignUpForm.tsx
@@ -1,28 +1,55 @@
 import { useState } from 'react'
 
+interface UserData {
+  authId: string
+  username: string
+  fullname: string
+  location: string
+  img: string
+}
+
 function SignUpForm() {
-  const [username, setUsername] = useState('')
+  const [username, setUsername] = useState({
+    authID: '',
+    username: '',
+    fullname: '',
+    location: '',
+    img: '',
+  })
 
   const handleChange = (
+    key: string,
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
-    const newusername = e.target.value
+    const newusername = { ...username, [key]: e.target.value }
     setUsername(newusername)
   }
 
   return (
     <div>
-      <form>
+      <form className="flex flex-col gap-2">
         <div>
           <label className="mx-2" htmlFor="username">
             Username
           </label>
           <input
-            onChange={(e) => handleChange(e)}
+            onChange={(e) => handleChange('username', e)}
             type="text"
             id="username"
             name="username"
-            value={username}
+            value={username.username}
+          />
+        </div>
+        <div>
+          <label className="mx-2" htmlFor="fullname">
+            Fullname
+          </label>
+          <input
+            onChange={(e) => handleChange('fullname', e)}
+            type="text"
+            id="fullname"
+            name="fullname"
+            value={username.fullname}
           />
         </div>
       </form>
