20320b79f7aa5a7d0bd11831277a809d56c18a42
Ash Nandan <ashr5nandan@gmail.com>
2025-11-06 16:00:45 +1300

    have a working News feature with api key

--- client/routes.tsx
+++ client/routes.tsx
@@ -1,10 +1,12 @@
 import { createRoutesFromElements, Route } from 'react-router'
 import App from './components/App.tsx'
 import ViewAllProfiles from './components/ViewAllProfiles.tsx'
+import News from './components/News.tsx'
 
 export default createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route index element={<ViewAllProfiles />} />
     <Route path="/profiles" element={<ViewAllProfiles />} />
+    <Route path="/news" element={<News />} />
   </Route>,
 )
